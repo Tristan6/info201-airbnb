207f4fdf3c5aeffb1685901b237c76fab5a2d776
adharshr <adharshest@gmail.com>
2018-03-07 18:18:25 -0800

    change plots to be returned from functions

--- server.R
+++ server.R
@@ -1,17 +1,15 @@
 source('pre-processing.R')
+source("question4.R")
 library("markdown")
 
 shinyServer(function(input, output, session) {
   
   current <- reactive({
     return(GetFrame(input$city, input$year))
-    
   })
   
   #### Question 1 space #####
   
-  
-
   #### Question 1 space #####
   
   #### Question 2 space #####
@@ -27,8 +25,21 @@ shinyServer(function(input, output, session) {
   
 
   #### Question 4 space #####
+  question4.data.frame <- reactive({
+    return(getDataFrame4(curent()))
+  })
   
- 
-  #### Question 4 space #####c
+  output$bar.graph4 <- renderPlot({
+    getBarGraphReviews(question4.data.frame())
+  })
+  
+  output$pie.chart4 <- renderPlot({
+    getPieReviews(question4.data.frame())
+  })
+  
+  output$scatter.plot4 <- renderPlot({
+    getScatterReviews(question4.data.frame())
+  }) 
+  #### Question 4 space #####
 
 })
